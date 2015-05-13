82883a0c8d2df1f2669599561350c89ec249e3e1
Simone Basso <bassosimone@gmail.com>
2015-05-13 12:55:37 +0200

    Create __init__.py
    Also add copyright info and docstring

---
+++ neubot_cdn_test/__init__.py
@@ -0,0 +1,8 @@
+#
+# This file is part of Neubot CDN test.
+#
+# Neubot CND test is free software. See AUTHORS and LICENSE for
+# more information on the copying conditions.
+#
+
+""" Neubot CND test module """
