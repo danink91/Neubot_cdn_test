af691d271991606f7347fe82f79b1a72fde7cd4a
danink91 <daniele.dicarlo91@gmail.com>
2015-05-15 00:38:35 +0200

    changes in reverse_lookup

--- reverse_lookup.py
+++ reverse_lookup.py
@@ -1,17 +1,37 @@
 import sys
-
+import json
 from twisted.internet import task
 from twisted.names import client
+
+class Answers(object):
+
+    def __init__(self, orig):
+        self.orig = orig
+
+    def __str__(self):
+        content = []
+        for elem in self.orig:
+            content.append({
+                "name": str(elem.name),
+                "type": getattr(elem, "type"),
+                "class": elem.cls,
+                "ttl": elem.ttl,
+                "auth": elem.auth,
+                "payload":[str(elem.payload.name),elem.payload.ttl,], 
+            })
+        return json.dumps(content, indent=2)
+
 
 def reverseNameFromIPAddress(address):
     return '.'.join(reversed(address.split('.'))) + '.in-addr.arpa'
 
 def printResult(result):
-    answers, authority, additional = result
-    if answers:
-        a = answers[0]
-	ans=str(a.payload).split(' ')[1]
-        print ans[5:]
+    answers=result
+
+    for x in answers:
+	    if x: 
+	        a=Answers(x)
+		print a
 
 def main(reactor, address):
     d = client.lookupPointer(name=reverseNameFromIPAddress(address=address))
