c9e27d9c81fe5bc553f477cb1ed63200af7c07ba
danink91 <daniele.dicarlo91@gmail.com>
2015-05-29 20:48:00 +0200

    add errback in reverse_lookup.py

--- neubot_cdn_test/reverse_lookup.py
+++ neubot_cdn_test/reverse_lookup.py
@@ -5,7 +5,30 @@
 # more information on the copying conditions.
 #
 
-""" Performs reverse lookup of an address """
+""" Performs reverse lookup of an address
+
+Example usage:
+
+from twisted.internet import reactor
+import sys
+def main():
+
+    deferred = reverse_lookup(sys.argv[1])
+
+    def print_result(result):
+        print result
+        reactor.stop()
+
+    def print_error(err):
+        print err
+        reactor.stop()
+
+    deferred.addCallbacks(print_result,print_error)
+    reactor.run()
+
+if __name__ == "__main__":
+    main()
+"""
 
 from twisted.internet import defer
 from twisted.names import client
@@ -17,13 +40,11 @@ import socket
 
 class ReverseAnswer(object):
     """This class is the list of Reverse Answers"""
-    def __init__(self,result):
-        self.ans = result[0]
-        self.auth = result[1]
-        self.additional = result[2]
+    def __init__(self, result):
+        self.result = result
     def __str__(self):
         content = []
-        for elem in self.ans:
+        for elem in self.result[0]:
             if elem.type == dns.PTR:
                 content.append({
                     "name" : str(elem.name),
@@ -39,47 +60,55 @@ class ReverseAnswer(object):
 
 class ReverseErrors(object):
     """This class is the list of Reverse Errors"""
-    def __init__(self,result):
-        self.message = result.value.message
-        
+    def __init__(self, result):
+        self.message = result
+
     def __str__(self):
         content = []
         content.append({
-            "id" : str(self.message.id),
-            "rCode" : str(self.message.rCode),
-            "maxSize": str(self.message.maxSize),
-            "answer": str(self.message.answer),
-            "recDes": str(self.message.recDes),
-            "recAv": str(self.message.recAv),
-            "queries": str(self.message.queries),
-            "authority": str(self.message.authority),
-            "opCode": str(self.message.opCode),
-            "ns": str(self.message.ns),
-            "auth": str(self.message.auth),
+            "id" : str(self.message.value.message.id),
+            "rCode" : str(self.message.value.message.rCode),
+            "maxSize": str(self.message.value.message.maxSize),
+            "answer": str(self.message.value.message.answer),
+            "recDes": str(self.message.value.message.recDes),
+            "recAv": str(self.message.value.message.recAv),
+            "queries": str(self.message.value.message.queries),
+            "authority": str(self.message.value.message.authority),
+            "opCode": str(self.message.value.message.opCode),
+            "ns": str(self.message.value.message.ns),
+            "auth": str(self.message.value.message.auth),
         })
         return json.dumps(content, indent=2)
+
+def reverse_ipv6(ipv6):
+
+    """Return the reverse DNS pointer name for the IPv6 address.
+    This implements the method described in RFC3596 2.5. """
+    reverse_chars = ipv6.exploded[::-1].replace(':', '')
+    return '.'.join(reverse_chars) + '.ip6.arpa'
+
+def reverse_ipv4(ipv4):
+
+    """Return the reverse DNS pointer name for the IPv4 address.
+    This implements the method described in RFC1035 3.5. """
+    reverse_octets = str(ipv4).split('.')[::-1]
+    return '.'.join(reverse_octets) + '.in-addr.arpa'
 
 def getquery(address):
     """Takes as input an address and processes ipv4 and ipv6 for DNSquery"""
     try:
-        socket.inet_aton(address)
-        return '.'.join(reversed(address.split('.'))) + '.in-addr.arpa'
+        return reverse_ipv4(address)
     except socket.error:
 
         try:
-            address =socket.inet_pton(socket.AF_INET6, address).encode('hex')
-            s=""    
-            for i in address:
-                s=s+"."+i
-            return s[::-1]+'ip6.arpa.'
+            return reverse_ipv6(address)
         except socket.error:
-            print "error"
             return "error"
 
 def reverse_ip_address(address):
     """This function takes as input an address gets the query"""
-    query=getquery(address)
-    return query     
+    query = getquery(address)
+    return query
 
 def reverse_lookup(address):
     """This function performs the Reverse Lookup for each ip of
@@ -90,13 +119,13 @@ def reverse_lookup(address):
         """ Wrap result returned by Twisted """
         outer_deferred.callback(ReverseAnswer(result))
 
-    def wrap_error(error):
-        x=ReverseErrors(error)
-        outer_deferred.callback(x)
+    def wrap_error(err):
+        """ Wrap error returned by Twisted """
+        outer_deferred.errback(ReverseErrors(err))
 
     rev_ip = reverse_ip_address(address=address)
     inner_deferred = client.lookupPointer(rev_ip)
-    inner_deferred.addCallbacks(wrap_result,wrap_error)
+    inner_deferred.addCallbacks(wrap_result, wrap_error)
     return outer_deferred
 
 def main():
@@ -110,7 +139,12 @@ def main():
         print result
         reactor.stop()
 
-    deferred.addCallback(print_result)
+    def print_error(err):
+        """ Print err of name lookup """
+        print err.value
+        reactor.stop()
+
+    deferred.addCallbacks(print_result,print_error)
     reactor.run()
 
 if __name__ == "__main__":
