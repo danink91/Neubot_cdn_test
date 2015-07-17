20f5479d31a57b016ff9c98733fe07310c936b58
danink91 <daniele.dicarlo91@gmail.com>
2015-07-17 17:45:47 +0200

    add end test time

--- bin/neubot-cdn-test.sh
+++ bin/neubot-cdn-test.sh
@@ -3,9 +3,10 @@ cd ..
 COUNTER=0
 while [ true ]; do
   COUNTER=$((COUNTER+1))
+  echo "start test: $COUNTER at"
   date
-  echo "start test: $COUNTER"
   python -m neubot_cdn_test $@
-  echo "end test: $COUNTER"
+  echo "end test: $COUNTER at"
+  date
   sleep 1800
 done
